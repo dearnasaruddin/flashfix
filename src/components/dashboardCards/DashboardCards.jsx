3b7066c95e570f9c955d30a3253c3f9374235585
dearnasaruddin <dearnasaruddin@gmail.com>
2026-01-30 02:11:00 +0600

    dashboardCard size fixed for mobile devices

--- src/components/dashboardCards/DashboardCards.jsx
+++ src/components/dashboardCards/DashboardCards.jsx
@@ -7,7 +7,7 @@ const DashboardCards = () => {
         <div className="flex items-center justify-center md:justify-between gap-2.5 md:gap-6 flex-wrap">
             {
                 dashboardCardsData.map((card, index) =>
-                    <div key={index} className="min-w-36 max-w-[48%] max-md:h-37.5 md:w-[31.2%] md:grow">
+                    <div key={index} className="min-w-34 max-w-[40%] max-md:h-37.5 md:w-[31.2%] md:grow">
                         <CardItem title={card.title} value={card.value} percent={card.percent} posIcon={card.icon} failed={card.failed} />
                     </div>
                 )
