428c18706170b9157df67b7e715e3f9bdb4d9eac
dearnasaruddin <dearnasaruddin@gmail.com>
2026-04-06 11:37:31 +0600

    pagination fixed

--- src/components/appointmentTable/Pagination.jsx
+++ src/components/appointmentTable/Pagination.jsx
@@ -13,16 +13,11 @@ const Pagination = ({ totalItems, itemPerPage, currentPage, onPageChange }) => {
                 <span className='font-medium max-sm:hidden'>Previous</span>
             </button>
 
-            {/* ============= First Page ============= */}
-            <button onClick={() => onPageChange(1)} className={`size-5 xs:size-8 md:size-10 flex justify-center items-center text-[#0F62FE] ${currentPage == 1 ? 'bg-[#A6C8FF]' : 'cursor-pointer hover:bg-primary'}`}>1</button>
-
-            {/* ============= Middle Pages with dots ============= */}
+            {/* ============= Pages with dots ============= */}
             {pageNumbers.map((pageNumber, index) => (
                 <button key={index} disabled={pageNumber == '...'} onClick={() => onPageChange(pageNumber)} className={`size-5 xs:size-8 md:size-10 text-[#0F62FE]  ${currentPage == pageNumber ? 'bg-[#A6C8FF]' : pageNumber != '...' && 'cursor-pointer hover:bg-primary'}`} >{pageNumber}</button>
             ))}
 
-            {/* ============= Last Page ============= */}
-            <button onClick={() => onPageChange(totalPages)} className={`size-5 xs:size-8 md:size-10 flex justify-center items-center text-[#0F62FE] ${currentPage == totalPages ? 'bg-[#A6C8FF]' : 'hover:bg-primary'}`}>{totalPages}</button>
 
             {/* ============= Next Btn ============= */}
             <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages} className={`flex items-center md:gap-2 p-2 ${currentPage == totalPages ? 'text-[#697077]' : 'text-[#0F62FE]'} cursor-pointer`}>
--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,39 +1,14 @@
-
-const usePagination = ({  totalItems,  itemPerPage,  currentPage,}) => {
+const usePagination = ({ totalItems, itemPerPage, currentPage, gap = 1 }) => {
 
   const totalPages = Math.ceil(totalItems / itemPerPage);
   const pageNumbers = [];
 
-  if (currentPage > 4 && currentPage < totalPages - 2) {
-    for (let i = currentPage - 1; i <= currentPage + 1; i++) {
-      pageNumbers.push(i);
+  for (let i = 1; i <= totalPages; i++) {
+    if (i === 1 || i === totalPages || (currentPage - gap <= i && i <= currentPage + gap)) {
+      pageNumbers.push(i)
+    } else if ((currentPage - gap - 1 === i) || (currentPage + gap + 1 === i)) {
+      pageNumbers.push('...');
     }
-    pageNumbers.unshift('...');
-    pageNumbers.push('...');
-  }
-
-  else if (currentPage > 4) {
-    for (
-      let i = currentPage - 2;
-      i < Math.min(totalPages, currentPage + 2);
-      i++
-    ) {
-      pageNumbers.push(i);
-    }
-    pageNumbers.unshift('...');
-  }
-
-  else if (currentPage < totalPages - 3) {
-    if (currentPage <= 3) {
-      for (let i = 2; i <= 5; i++) {
-        pageNumbers.push(i);
-      }
-    } else {
-      for (let i = 2; i <= currentPage + 1; i++) {
-        pageNumbers.push(i);
-      }
-    }
-    pageNumbers.push('...');
   }
 
   return {
